e29558f0ba94794630d2c08eface2448650f5e0e
nghiemph95 <nghiemph95@gmail.com>
2022-11-03 14:42:26 +0700

    Updated: 03-11-2022 14:42:26

--- 04_Props&Composition/01_props.jsx
+++ 04_Props&Composition/01_props.jsx
@@ -1,6 +1,6 @@
 /** Props:
  * - Dữ liệu được truyền từ components cha xún components con
  * và con sẽ nhận được dữ liệu từ cha nhưng không thay đổi được dữ liệu nó nhận được
- * - Nhờ có Props mà nó tạo được sự đa dạng cho components. Cùng một component với props khác nhau thì
+ * - Nhờ có Props mà nó tạo được sự đa dạng(composition) cho components. Cùng một component với props khác nhau thì
  * render ra khác nhau
  */
