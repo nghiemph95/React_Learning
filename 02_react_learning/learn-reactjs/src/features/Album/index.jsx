39e29fd7d51404b5baed937e8ee38efb9af9ef7e
nghiemph95 <nghiemph95@gmail.com>
2022-11-04 14:04:10 +0700

    Updated: 04-11-2022 14:04:10

--- 02_react_learning/learn-reactjs/src/features/Album/index.jsx
+++ 02_react_learning/learn-reactjs/src/features/Album/index.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
-import TodoList from "./components/TodoList";
 import AlbumList from "./components/AlbumList";
 
 AlbumFeature.propTypes = {};
@@ -16,13 +14,13 @@ function AlbumFeature(props) {
     {
       id: 2,
       name: "Rap Việt",
-      title:
+      thumbnailUrl:
         "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/1/0/d/d/10dd6f469f1e054fb88fe101af95ecd8.jpg",
     },
     {
       id: 3,
       name: "Trào Lưu Đang Hot",
-      title:
+      thumbnailUrl:
         "https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/8/8/5/0/88507fb5f1862276c5b696eb090eb72b.jpg",
     },
   ];
