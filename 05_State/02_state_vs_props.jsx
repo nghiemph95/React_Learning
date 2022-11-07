b3f31cecbe864fe400988de7c4ee4ff10a798c10
nghiemph95 <nghiemph95@gmail.com>
2022-11-07 23:27:11 +0700

    Updated: 07-11-2022 23:27:11

---
+++ 05_State/02_state_vs_props.jsx
@@ -0,0 +1,10 @@
+/**
+ * Props vs State:
+ *
+ * -Truyền dữ liệu từ cha xún con: Props
+ *
+ * -Truyền dữ liệu giữa component ngang hàng: Để state trên
+ * component cha, r từ cha truyền xún component con
+ *
+ * -Truyền dữ liệu giữa các trang khác nhau: Dùng Redux
+ */
