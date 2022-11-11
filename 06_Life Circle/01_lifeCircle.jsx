142e1dbb9f0168ecf286878506033eddf4b7c747
nghiemph95 <nghiemph95@gmail.com>
2022-11-12 01:58:10 +0700

    Updated: 12-11-2022 01:58:10

---
+++ 06_Life Circle/01_lifeCircle.jsx
@@ -0,0 +1,12 @@
+/** Life Circle(Vòng đời của 1 Component) chỉ đc apply đối với Class Component
+ */
+
+/** Life Circle
+ * 1. Mounting: Được tạo ra
+ * 2. Updating: Cập nhật thay đổi
+ * 3. Unmounting: Hủy bỏ
+ */
+
+/** Nên dùng PureComponent vì nó sẽ so sánh props và state trước và sau để
+ * nếu có thay đổi thì nó mới render
+ */
