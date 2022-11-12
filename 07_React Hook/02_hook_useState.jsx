0356dcea43e63847b86f18db665b57ed9fc7252d
nghiemph95 <nghiemph95@gmail.com>
2022-11-12 14:13:04 +0700

    Updated: 12-11-2022 14:13:04

--- 07_React Hook/02_hook_useState.jsx
+++ 07_React Hook/02_hook_useState.jsx
@@ -3,3 +3,58 @@
  * - Input: initialState (giá trị khởi tạo của state hoặc cb function)
  * - Output: 1 mảng gồm 2 phần tử là state và setState(hàm giúp mình có thể cập nhật giá trị mới cho state)
  * - Cách dùng: const [name, setName] = useState('Nghiêm')*/
+
+const { useState } = require("react");
+
+/** useState example */
+function TodoList() {
+  const [todoList, setTodoList] = useState(["love", "easy", "test"]);
+
+  function removeTodo(index) {
+    const newTodoList = [...todoList];
+
+    newTodoList.splice(index, 1); // Xóa 1 phần tử ở vị trí index
+    setTodoList(newTodoList);
+  }
+
+  return (
+    <ul className="todo-list">
+      {todoList.map((todo, index) => {
+        <li
+          key={todo.id}
+          onClick={() => {
+            removeTodo(index);
+          }}
+        >
+          {todo.title}
+        </li>;
+      })}
+    </ul>
+  );
+}
+
+/** Lưu ý khi dùng state:
+ * - useState() sử dụng REPLACING thay vì MERGING
+ */
+
+// setState() in class component: MERGING
+this.state = { name: "Test", color: "red" };
+this.setState({ color: "green" }); // -> {name: "test", color: 'green'}
+
+// useState() in functional component: REPLACING
+const [person, setPerson] = useState({ name: "Test", color: "red" });
+setPerson({ color: "green" }); // -> {color: 'green'}
+setPerson({ ...person, color: "green" });
+
+/** Lưu ý khi dùng state:
+ * - Initial state chỉ dùng cho lần đầu , những lần sau nó bị bỏ rơi
+ * * Nên là nếu muốn sử dụng nó 1 lần duy nhất thì sử dụng callback
+ */
+
+function ColorBox() {
+  const [color, setColor] = useState(() => {
+    // Sử dụng callback và function sẽ được call 1 lần
+    const initColor = getComplicatedColor();
+    return initColor;
+  });
+}
