a16d9937153a489d48263578596ea8953ce15a44
nghiemph95 <nghiemph95@gmail.com>
2022-11-12 14:38:14 +0700

    Updated: 12-11-2022 14:38:14

---
+++ 07_React Hook/03_useEffect.jsx
@@ -0,0 +1,18 @@
+/** sideEffect là gì: (nôm na là những thứ ngoài component)
+ * - gọi API để lấy dữ liệu
+ * - tương tác với DOM
+ * - Subscription
+ * - setTimeout, setInterval
+ *
+ * ** Có 2 loại side effect:
+ * - Effects không cần clean up: gọi API, tương tác DOM
+ * - Effects cần clean up: subscriptions, setTimeout, setInterval
+ */
+
+/** useEffect() là gìL
+ * - Là 1 cái hook trong React Hook
+ * - Sử dụng cho FC để sử dụng side effect
+ * - Mỗi hook useEffect gồm 2 thành phần: side effect và clean up (optional)
+ * - THỰC THI SAU MỖI LẦN RENDER
+ * - Tuy nhiên nó còn có dependency và sau mỗi lần render nó còn TÙY THUỘC VÀO DEPENDENCY
+ */
