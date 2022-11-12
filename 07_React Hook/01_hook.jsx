ba2e1f5cb3b158c393aa23ba0c73cd98fa52e3cb
nghiemph95 <nghiemph95@gmail.com>
2022-11-12 11:52:59 +0700

    Updated: 12-11-2022 11:52:59

---
+++ 07_React Hook/01_hook.jsx
@@ -0,0 +1,8 @@
+/** React Hook là gì:
+ * Feature mới đc add từ version 16.8 của React
+ * Giúp cho FC powerfull hơn (có thẻ dùng State, life Cycle và các feature khác mà ko cần dùng tới class)
+ */
+
+/** Khi nào xài Hook (chỉ sử dụng đối với Functional Component)
+ * - Khi muôn sử dụng state, life cycle, ... mà ko thích làm việc với class (OOP)
+ */
