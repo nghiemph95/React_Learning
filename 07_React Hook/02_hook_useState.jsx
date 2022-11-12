4c44cfaacb58df7411ba9f2d633e07dcd69abc92
nghiemph95 <nghiemph95@gmail.com>
2022-11-12 13:47:04 +0700

    Updated: 12-11-2022 13:47:04

--- 07_React Hook/02_hook_useState.jsx
+++ 07_React Hook/02_hook_useState.jsx
@@ -1,5 +1,5 @@
 /** useState chính xác là một hook cơ bản:
  * - Giúp cho FC có thể sử dụng state
  * - Input: initialState (giá trị khởi tạo của state hoặc cb function)
- * - Output: 1 mảng gồm 2 phần tử là state và setState
+ * - Output: 1 mảng gồm 2 phần tử là state và setState(hàm giúp mình có thể cập nhật giá trị mới cho state)
  * - Cách dùng: const [name, setName] = useState('Nghiêm')*/
