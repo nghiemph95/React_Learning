6d1598a29600f1e5507aeea65067a5066989d716
nghiemph95 <nghiemph95@gmail.com>
2022-11-13 21:58:43 +0700

    Updated: 13-11-2022 21:58:43

--- 07_React Hook/04_customHook.jsx
+++ 07_React Hook/04_customHook.jsx
@@ -8,7 +8,7 @@
  * vừa có thể sử dụng giữa nhiều loại hook khác nhau
  */
 
-const { useEffect } = require("react");
+const { useEffect, useState } = require("react");
 
 /** Đối với FC thì React Hook return về JSX/TSX còn đối
  * với custom Hook thì lại return về Data, Dữ liệu, biến, object, array...
@@ -42,4 +42,68 @@ function MaginBox() {
   const color = useMaginColor(); // Sử dụng 1 logic đã được viết bên ngoài
 
   return <div class="magic-box" style={{ backgroundColor: color }}></div>;
+}
+
+/** Ví dụ sử dụng custom Hook đơn giản */
+// Bước 1: Từ hàm reder bình thường sử dụng react Hook
+function Clock() {
+  const [timeString, setTimeString] = useState("");
+
+  useEffect(() => {
+    const clockInterval = setInterval(() => {
+      const now = new Date();
+      // HH:mm:ss
+      const newTimeString = formatDate(now);
+
+      setTimeString(newTimeString);
+    }, 1000);
+
+    return () => {
+      //clean up
+      console.log("Clock cleanup");
+      clearInterval(clockInterval);
+    };
+  }, []);
+
+  return <p style={{ fontSize: "42px" }}>{timeString}</p>;
+}
+
+// Bước 2: Copy toàn bộ hàm bên trên và đổi tên thành useClock()
+function useClock() {
+  const [timeString, setTimeString] = useState("");
+
+  useEffect(() => {
+    const clockInterval = setInterval(() => {
+      const now = new Date();
+      // HH:mm:ss
+      const newTimeString = formatDate(now);
+
+      setTimeString(newTimeString);
+    }, 1000);
+
+    return () => {
+      //clean up
+      console.log("Clock cleanup");
+      clearInterval(clockInterval);
+    };
+  }, []);
+
+  return { timeString };
+}
+
+// Bước 3: Gắn customHook vào các function đã được định
+function Clock() {
+  const { timeString } = useClock();
+
+  return <p style={{ fontSize: "42px" }}>{timeString}</p>;
+}
+
+function BetterClock() {
+  const { timeString } = useClock();
+
+  return (
+    <div className="better-clock">
+      <p className="better-clock__time">{timeString}</p>
+    </div>
+  );
 }
