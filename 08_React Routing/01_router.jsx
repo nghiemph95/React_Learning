1a448776dc86d144d31b0abd340743ea05b807b5
nghiemph95 <nghiemph95@gmail.com>
2022-11-15 23:10:16 +0700

    Updated: 15-11-2022 23:10:16

---
+++ 08_React Routing/01_router.jsx
@@ -0,0 +1,10 @@
+/** Routing: Người ta đi vào đường dẫn thì nó sẽ bik cần render router nào
+ *
+ * Component:
+ * - Router: Component bự nhất, bao bọc tất cả các component khác của routing
+ * - Route: Quy định đường dẫn này thì render ở route nào
+ * - Switch: Chỉ render route đầu tiên match vs đường dẫn hiện tại
+ * - Redirect: Chuyển hướng đường dẫn khác
+ * - Link: Đi tới đướng dẫn tương ứng
+ * - NavLink: Giống như Menu, khi đi đến đường dẫn trùng khớp vs link hiện tại (có thêm ClassActive)
+ */
