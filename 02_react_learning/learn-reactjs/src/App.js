c3033f57d12bfd16e8ecec2e362638121c8f7dfc
nghiemph95 <nghiemph95@gmail.com>
2022-11-15 23:43:51 +0700

    Updated: 15-11-2022 23:43:51

--- 02_react_learning/learn-reactjs/src/App.js
+++ 02_react_learning/learn-reactjs/src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import AlbumFeature from "./features/Album";
 import React from "react";
 import TodoFeature from "./features/Todo";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <TodoFeature />
-        {/* <AlbumFeature /> */}
-      </header>
+      {/* <header className="App-header"> */}
+      {/* <TodoFeature /> */}
+      {/* <AlbumFeature /> */}
+      {/* </header> */}
+
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/albums" component={AlbumFeature} />
     </div>
   );
 }
