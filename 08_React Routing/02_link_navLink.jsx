83b613dd931e4700904a2abe35c2fb1226968f1e
nghiemph95 <nghiemph95@gmail.com>
2022-11-22 14:51:44 +0700

    Updated: 22-11-2022 14:51:44

---
+++ 08_React Routing/02_link_navLink.jsx
@@ -0,0 +1,5 @@
+/** Phân biệt Link và NavLink
+ * Link: Dùng để chuyển đến trang mình muốn
+ * Navlink: Dùng để khi muốn làm dạng menu, style cho
+ * các trang đang đc active, dựa vào class để style cho các link khi đc active.
+ */
