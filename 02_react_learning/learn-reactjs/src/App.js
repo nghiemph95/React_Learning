9b91bc54884b6e2c53c9bf10af23cd080857955b
nghiemph95 <nghiemph95@gmail.com>
2022-11-22 14:39:28 +0700

    Updated: 22-11-2022 14:39:28

--- 02_react_learning/learn-reactjs/src/App.js
+++ 02_react_learning/learn-reactjs/src/App.js
@@ -1,8 +1,8 @@
+import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import AlbumFeature from "./features/Album";
-import React from "react";
 import TodoFeature from "./features/Todo";
-import { Route } from "react-router-dom";
 
 //Mainnn
 function App() {
