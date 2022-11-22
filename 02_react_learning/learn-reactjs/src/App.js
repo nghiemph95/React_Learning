34164b616755bfe42edc0a2c2eae21459a347dc4
nghiemph95 <nghiemph95@gmail.com>
2022-11-22 14:58:30 +0700

    Updated: 22-11-2022 14:58:30

--- 02_react_learning/learn-reactjs/src/App.js
+++ 02_react_learning/learn-reactjs/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, NavLink, Route } from "react-router-dom";
 import "./App.css";
 import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
@@ -13,6 +13,22 @@ function App() {
       {/* <AlbumFeature /> */}
       {/* </header> */}
       header
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/albums">Albums</Link>
+      </p>
+      <p>
+        <NavLink to="/todos" activeClassName="active-menu">
+          Todos
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to="/albums" activeClassName="active-menu">
+          Albums
+        </NavLink>
+      </p>
       <Route path="/todos" component={TodoFeature} />
       <Route path="/albums" component={AlbumFeature} />
       footer
