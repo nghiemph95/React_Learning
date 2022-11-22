5ad7aa456f3d4c9415b850bc635c752931416dc1
nghiemph95 <nghiemph95@gmail.com>
2022-11-23 00:34:53 +0700

    Updated: 23-11-2022 00:34:53

--- 02_react_learning/learn-reactjs/src/features/Todo/index.jsx
+++ 02_react_learning/learn-reactjs/src/features/Todo/index.jsx
@@ -1,74 +1,17 @@
 import React from "react";
-import { useState } from "react";
-import TodoList from "./components/TodoList";
+import { Route, Switch } from "react-router-dom";
+import DetailPage from "./pages/DetailPage";
+import ListPage from "./pages/ListPage";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const initTodoList = [
-    {
-      id: 1,
-      title: "Eat",
-      status: "new",
-    },
-    {
-      id: 2,
-      title: "Sleep",
-      status: "completed",
-    },
-    {
-      id: 3,
-      title: "Code",
-      status: "new",
-    },
-  ];
-
-  const [todoList, setTodoList] = useState(initTodoList);
-  const [filterStatus, setFilterStatus] = useState("all");
-
-  const handleTodoClickFromChildren = (todo, idx) => {
-    console.log(todo, idx);
-    // clone current array
-    const newTodoList = [...todoList];
-    // toggle state
-    newTodoList[idx] = {
-      ...newTodoList[idx],
-      status: newTodoList[idx].status === "new" ? "completed" : "new",
-    };
-
-    // update todolis
-    setTodoList(newTodoList);
-  };
-
-  const handleShowAllClick = () => {
-    setFilterStatus("all");
-  };
-
-  const handleShowCompletedClick = () => {
-    setFilterStatus("completed");
-  };
-
-  const handleShowNewClick = () => {
-    setFilterStatus("new");
-  };
-
-  const filterTodoList = todoList.filter(
-    (element) => filterStatus === "all" || element.status === filterStatus
-  );
-
   return (
     <div>
-      <h3>TodoList</h3>
-      <TodoList
-        todoList={filterTodoList}
-        onTodoClick={handleTodoClickFromChildren}
-      />
-
-      <div>
-        <button onClick={handleShowAllClick}>Show All</button>
-        <button onClick={handleShowCompletedClick}>Show Completed</button>
-        <button onClick={handleShowNewClick}>Show New</button>
-      </div>
+      <Switch>
+        <Route path="/todos" component={ListPage} />
+        <Route path="/todos/:todoId" component={DetailPage} />
+      </Switch>
     </div>
   );
 }
