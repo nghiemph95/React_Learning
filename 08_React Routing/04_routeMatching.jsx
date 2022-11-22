6cce238e2a5050328d972b4101b1b4118046c242
nghiemph95 <nghiemph95@gmail.com>
2022-11-22 17:00:12 +0700

    Updated: 22-11-2022 17:00:12

--- 08_React Routing/04_routeMatching.jsx
+++ 08_React Routing/04_routeMatching.jsx
@@ -4,16 +4,16 @@
  */
 
 /** Ví dụ:
- * exact  path            URL        Match
- * false  /               /todos     YES
- * false  /               /albums    YES
- * false  /todos          /albums    NO
- * false  /todos          /todos     YES
- * false  /todos          /todos/123 YES
- * true   /               /          YES
- * true   /               /todos     NO
- * true   /               /albums    NO
- * true   /todos          /todos     YES
- * true   /todos          /todos/123 NO
- * true   /todos/:todoId  /todos/123 YES
+ * exact  path            URL         Match
+ * false  /               /todos      YES
+ * false  /               /albums     YES
+ * false  /todos          /albums     NO
+ * false  /todos          /todos      YES
+ * false  /todos          /todos/123  YES
+ * true   /               /           YES
+ * true   /               /todos      NO
+ * true   /               /albums     NO
+ * true   /todos          /todos      YES
+ * true   /todos          /todos/123  NO
+ * true   /todos/:todoId  /todos/123  YES
  */
