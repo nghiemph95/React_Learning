a578f0cde661323bc9b46a98b4a12bda3daab128
nghiemph95 <nghiemph95@gmail.com>
2022-11-27 22:09:50 +0700

    Updated: 27-11-2022 22:09:50

--- 02_react_learning/learn-reactjs/src/features/Todo/components/TodoForm/index.jsx
+++ 02_react_learning/learn-reactjs/src/features/Todo/components/TodoForm/index.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import InputField from "../../../../components/form-controls/InputField";
+import { useForm } from "react-hook-form";
 
 TodoForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
-  const form = useForm;
+  const form = useForm({
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const handleSubmit = (values) => {
+    console.log("Todo Form: ", values);
+  };
 
   return (
-    <div>
-      todoForm
-      <InputField />
-    </div>
+    <form onSubmit={form.handleSubmit(handleSubmit)}>
+      <InputField name="title" label="Todo" form={form} />
+    </form>
   );
 }
 
