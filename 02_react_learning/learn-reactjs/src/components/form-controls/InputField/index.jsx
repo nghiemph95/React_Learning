1ebad97316222503dd70320d1c9e0290fb4763c6
nghiemph95 <nghiemph95@gmail.com>
2022-11-28 21:19:44 +0700

    Updated: 28-11-2022 21:19:44

--- 02_react_learning/learn-reactjs/src/components/form-controls/InputField/index.jsx
+++ 02_react_learning/learn-reactjs/src/components/form-controls/InputField/index.jsx
@@ -1,7 +1,14 @@
 import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
 import React from "react";
 
-InputField.propTypes = {};
+InputField.propTypes = {
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+};
 
 function InputField(props) {
   return (
