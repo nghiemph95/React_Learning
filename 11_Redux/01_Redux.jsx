4206a57d876dd56791a5e82d3801009eaa0b93d0
nghiemph95 <nghiemph95@gmail.com>
2022-12-07 16:29:20 +0700

    Updated: 07-12-2022 16:29:20

--- 11_Redux/01_Redux.jsx
+++ 11_Redux/01_Redux.jsx
@@ -26,3 +26,5 @@
  *
  * Vì cùng 1 cái State, 1 cái Action thì nó luôn luôn cho ra 1 state mới giống nhau
  */
+
+/** mutate data: chỉnh sửa trực tiếp trên data (object, array) mà ko phải clone ra */
