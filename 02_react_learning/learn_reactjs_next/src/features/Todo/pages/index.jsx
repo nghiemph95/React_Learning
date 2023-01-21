1cd4f95c803a1c700a767146e25a204b81e748a8
Nghiem Pham <nghiemph95@gmail.com>
2023-01-21 17:10:02 +0700

    Updated: 21-01-2023 17:10:02

--- 02_react_learning/learn_reactjs_next/src/features/Todo/pages/index.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Todo/pages/index.jsx
@@ -26,6 +26,7 @@ function TodoFeature(props) {
 
   /** State */
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filterStatus, setFilterStatus] = useState('all');
 
   const handleTodoClick = (todo, idx) => {
     // clone current array to new one
@@ -43,10 +44,30 @@ function TodoFeature(props) {
     setTodoList(newTodoList);
   };
 
+  const handleShowAllClick = () => {
+    setFilterStatus('all');
+  };
+
+  const handleShowCompletedClick = () => {
+    setFilterStatus('completed');
+  };
+
+  const handleShowNewClick = () => {
+    setFilterStatus('new');
+  };
+
+  const renderTodoList = todoList.filter((todo) => filterStatus === 'all' || filterStatus === todo.status);
+
   return (
     <div>
       <h3> Todo List</h3>
-      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderTodoList} onTodoClick={handleTodoClick} />
+
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
     </div>
   );
 }
