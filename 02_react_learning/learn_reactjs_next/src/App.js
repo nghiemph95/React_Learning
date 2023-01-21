bcb395c9de78e3c7e87679ddc61d9e9af7c6a002
Nghiem Pham <nghiemph95@gmail.com>
2023-01-21 23:50:52 +0700

    Updated: 21-01-2023 23:50:52

--- 02_react_learning/learn_reactjs_next/src/App.js
+++ 02_react_learning/learn_reactjs_next/src/App.js
@@ -1,12 +1,24 @@
+import productApi from 'api/productApi';
 import NotFound from 'components/NotFound';
 import AlbumFeature from 'features/Album';
 import TodoFeature from 'features/Todo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 /** App main*/
 function App() {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const params = {
+        _limit: 10,
+      };
+      const productList = await productApi.getAll(params);
+      console.log(productList);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div className="app">
       Header
