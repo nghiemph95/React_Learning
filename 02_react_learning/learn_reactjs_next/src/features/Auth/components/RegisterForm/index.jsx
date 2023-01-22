da45fb0d121d2198f9f0f931160831d35d0509c4
Nghiem Pham <nghiemph95@gmail.com>
2023-01-22 21:07:28 +0700

    Updated: 22-01-2023 21:07:27

---
+++ 02_react_learning/learn_reactjs_next/src/features/Auth/components/RegisterForm/index.jsx
@@ -0,0 +1,44 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import InputField from 'components/form-controls/InputField';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+RegisterForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+function RegisterForm(props) {
+  /** Validation */
+  const schema = yup.object().shape({
+    title: yup.string().required('Please enter title!').min(5, 'Title is to short'),
+  });
+
+  const form = useForm({
+    /** Nếu không khai báo defaultValue thì nó sẽ hiểu là undefined (uncontrol) */
+    defaultValues: {
+      title: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  /** Handle Submit */
+  const handleSubmit = (values) => {
+    const { onSubmit } = props;
+
+    if (onSubmit) {
+      onSubmit(values);
+    }
+
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(handleSubmit)}>
+      <InputField name="title" form={form} label="Todo" />
+    </form>
+  );
+}
+
+export default RegisterForm;
