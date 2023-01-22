f5e39a321947170fa975ba3576eadaa89503af97
Nghiem Pham <nghiemph95@gmail.com>
2023-01-22 22:13:19 +0700

    Updated: 22-01-2023 22:13:19

--- 02_react_learning/learn_reactjs_next/src/components/form-controls/PasswordField/index.jsx
+++ 02_react_learning/learn_reactjs_next/src/components/form-controls/PasswordField/index.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TextField } from '@material-ui/core';
-import { Controller } from 'react-hook-form';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { useState } from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Controller } from 'react-hook-form';
 
 PasswordField.propTypes = {
   form: PropTypes.object.isRequired,
