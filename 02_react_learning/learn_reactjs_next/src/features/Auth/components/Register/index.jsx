b19556f5573b14e6e7b5a36ce0210760c9488bbe
Nghiem Pham <nghiemph95@gmail.com>
2023-01-23 13:47:29 +0700

    Updated: 23-01-2023 13:47:29

--- 02_react_learning/learn_reactjs_next/src/features/Auth/components/Register/index.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Auth/components/Register/index.jsx
@@ -1,14 +1,19 @@
 import { unwrapResult } from '@reduxjs/toolkit';
 import { register } from 'features/Auth/userSlice';
+import { useSnackbar } from 'notistack';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import RegisterForm from '../RegisterForm';
 
-Register.propTypes = {};
+Register.propTypes = {
+  closeDialog: PropTypes.func,
+};
 
 function Register(props) {
   // get dispatch
   const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = async (values) => {
     try {
@@ -20,7 +25,13 @@ function Register(props) {
       const resultAction = await dispatch(action);
       // get result after dispatch
       const user = unwrapResult(resultAction);
+
+      //close digalog when complete
+      const { closeDialog } = props;
+      if (closeDialog) closeDialog();
+
       console.log('New user', user);
+      enqueueSnackbar('Register completed', { variant: 'success' });
     } catch (error) {
       console.log('Failed to register: ', error);
     }
