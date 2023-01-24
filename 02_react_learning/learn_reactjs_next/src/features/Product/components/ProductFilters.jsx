f3ed397b4aaae2b500167f1b5f2c8d6e81c5a18a
Nghiem Pham <nghiemph95@gmail.com>
2023-01-24 17:12:24 +0700

    Updated: 24-01-2023 17:12:24

--- 02_react_learning/learn_reactjs_next/src/features/Product/components/ProductFilters.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Product/components/ProductFilters.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import FilterByCategory from './Filters/FilterByCategory';
+import { Box } from '@material-ui/core';
 
 ProductFilters.propTypes = {
   filters: PropTypes.object.isRequired,
   onChange: PropTypes.func,
 };
 
-function ProductFilters(props) {
-  return <div>Product Filters</div>;
+function ProductFilters({ filters, onChange }) {
+  /** Khi FilterByCategory thay đổi, nó sẽ truyền newCategoryId lên handleCategoryChange và
+   * sau đó báo lên thằng cha
+   */
+  const handleCategoryChange = (newCategoryId) => {
+    if (!onChange) return;
+
+    const newFilters = {
+      ...filters,
+      categoryId: newCategoryId,
+    };
+
+    onChange(newFilters);
+  };
+
+  return (
+    <Box>
+      <FilterByCategory onChange={handleCategoryChange} />
+    </Box>
+  );
 }
 
 export default ProductFilters;
