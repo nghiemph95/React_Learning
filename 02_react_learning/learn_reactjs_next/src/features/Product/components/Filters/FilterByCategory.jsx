e1f25af70b943b8a182ba92efd2bf65bf7393851
Nghiem Pham <nghiemph95@gmail.com>
2023-01-24 17:36:17 +0700

    Updated: 24-01-2023 17:36:17

--- 02_react_learning/learn_reactjs_next/src/features/Product/components/Filters/FilterByCategory.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Product/components/Filters/FilterByCategory.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Box, Typography } from '@material-ui/core';
+import { useEffect } from 'react';
+import categoryApi from 'api/categoryApi';
+import { info } from 'node-sass';
 
-FilterByCategory.propTypes = {};
+FilterByCategory.propTypes = {
+  onChange: PropTypes.func,
+};
 
-function FilterByCategory(props) {
-  return <div>Filtee By Category</div>;
+function FilterByCategory({ onChange }) {
+  /** State */
+  const [categoryList, setCategoryList] = useState([]);
+
+  /** Query category data */
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await categoryApi.getAll();
+        console.log({ response });
+      } catch (error) {
+        console.log('Failed to query data', error);
+      }
+    })();
+  }, []);
+
+  /** Controller */
+  // const handleCategoryClick = (category) => {
+  //   if (onChange) {
+  //     onChange(category.id);
+  //   }
+  // };
+
+  return (
+    <Box>
+      <Typography>DANH MUC SAN PHAM</Typography>
+
+      <ul>
+        {categoryList.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </Box>
+  );
 }
 
 export default FilterByCategory;
--- 02_react_learning/learn_reactjs_next/src/features/Product/components/ProductFilters.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Product/components/ProductFilters.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FilterByCategory from './Filters/FilterByCategory';
 import { Box } from '@material-ui/core';
+import FilterByPrice from './Filters/FilterByPrice';
 
 ProductFilters.propTypes = {
   filters: PropTypes.object.isRequired,
