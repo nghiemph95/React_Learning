83e82b994e9f10845d6fd697559fdc91bb4f03a5
Nghiem Pham <nghiemph95@gmail.com>
2023-01-24 11:29:07 +0700

    Updated: 24-01-2023 11:29:07

--- 02_react_learning/learn_reactjs_next/src/features/Product/components/Product.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Product/components/Product.jsx
@@ -19,7 +19,7 @@ function Product({ product }) {
       <Typography variant="body2">{product.name}</Typography>
       <Typography variant="body2">
         <Box component="span" fontSize="16px" fontWeight="bold" mr={1}>
-          {product.salePrice}
+          {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
         </Box>
         {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
       </Typography>
