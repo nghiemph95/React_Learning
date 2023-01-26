45a77feff82bd1381b48b79026528984d749ab34
Nghiem Pham <nghiemph95@gmail.com>
2023-01-26 15:00:28 +0700

    Updated: 26-01-2023 15:00:28

--- 02_react_learning/learn_reactjs_next/src/features/Product/components/Product.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Product/components/Product.jsx
@@ -2,6 +2,7 @@ import { Box, Typography } from '@material-ui/core';
 import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'contants';
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 Product.propTypes = {
   product: PropTypes.object,
@@ -10,8 +11,14 @@ Product.propTypes = {
 function Product({ product }) {
   const thumbnailUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
 
+  /** Sử dụng history để navigate */
+  const history = useHistory();
+  const handleClick = () => {
+    history.push(`/products/${product.id}`);
+  };
+
   return (
-    <Box padding={1}>
+    <Box padding={1} onClick={handleClick}>
       <Box padding={1} minHeight="215px">
         <img src={thumbnailUrl} alt={product.name} width="100%" />
       </Box>
