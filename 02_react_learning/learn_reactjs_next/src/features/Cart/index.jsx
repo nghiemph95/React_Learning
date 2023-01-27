1462f527c85edc40e673f50968c63845d2b9cacf
Nghiem Pham <nghiemph95@gmail.com>
2023-01-27 22:10:40 +0700

    Updated: 27-01-2023 22:10:40

--- 02_react_learning/learn_reactjs_next/src/features/Cart/index.jsx
+++ 02_react_learning/learn_reactjs_next/src/features/Cart/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { cartTotalSelector } from './selectors';
-import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { cartItemsCountSelector, cartTotalSelector } from './selectors';
+import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 
 CartFeature.propTypes = {};
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(1),
   },
 
   left: {
@@ -21,6 +21,10 @@ const useStyles = makeStyles((theme) => ({
     flex: '1 1 0',
     padding: theme.spacing(1.5),
   },
+
+  header: {
+    margin: theme.spacing(1, 0),
+  },
 }));
 
 function CartFeature(props) {
@@ -28,10 +32,17 @@ function CartFeature(props) {
   const classes = useStyles();
 
   const cartTotalAmount = useSelector(cartTotalSelector);
+  const cartCountProduct = useSelector(cartItemsCountSelector);
 
   return (
     <Box className={classes.root}>
       <Container>
+        <Grid container>
+          <Grid item className={classes.header}>
+            <Typography>GIỎ HÀNG ({`${cartCountProduct}`} sản phẩm)</Typography>
+          </Grid>
+        </Grid>
+
         <Paper elevation={0}>
           <Grid container>
             <Grid item className={classes.left}>
