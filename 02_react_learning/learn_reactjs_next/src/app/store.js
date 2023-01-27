a4804a80fe84bd5a995e9e59c0935061e0829e67
Nghiem Pham <nghiemph95@gmail.com>
2023-01-27 15:24:44 +0700

    Updated: 27-01-2023 15:24:44

--- 02_react_learning/learn_reactjs_next/src/app/store.js
+++ 02_react_learning/learn_reactjs_next/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/Counter/counterSlice';
 import userReducer from '../features/Auth/userSlice';
+import createReducer from '../features/Cart/cartSlice';
 
 const rootReducer = {
   count: counterReducer,
   user: userReducer,
+  cart: createReducer,
 };
 
 const store = configureStore({
--- 02_react_learning/learn_reactjs_next/src/features/Cart/cartSlice.js
+++ 02_react_learning/learn_reactjs_next/src/features/Cart/cartSlice.js
@@ -10,8 +10,46 @@ const cartSlice = createSlice({
     showMiniCart(state, action) {
       state.showMiniCart = true;
     },
+
     hideMiniCart(state, action) {
       state.showMiniCart = false;
+    },
+
+    addToCart(state, action) {
+      /** Có 2 TH: Add mới và update số lượng hiện tại */
+      const newItem = action.payload; // truyền lên thông tin đầy đủ (id, product, quantity)
+      const index = state.cartItems.findIndex((x) => x.id === newItem.id);
+
+      if (index >= 0) {
+        /** Update new */
+        state.cartItems[index].quantity += newItem.quantity;
+      } else {
+        /** Add new */
+        state.cartItems.push(newItem);
+      }
+    },
+
+    /** Cập nhật số lượng sản phẩm */
+    setQuantify(state, action) {
+      /** Truyền lên 1 cái payload có thông tin id và quantity */
+      const { id, quantity } = action.payload;
+
+      /** Check if product is available in cart */
+      const index = state.cartItems.findIndex((x) => x.id === id);
+
+      if (index >= 0) {
+        state.cartItems[index].quantity = quantity;
+      }
+    },
+
+    removeFromCart(state, action) {
+      const idNeedToRemove = action.payload;
+
+      /** Hàm filter sẽ trả về 1 mảng mới, mảng mới sẽ bỏ vô cartItem
+       * do state ban đầu ở dạng object, nên nó sẽ hỗ trợ đc ImmerJS nên có thể
+       * mutate state trực tiếp mà không cần phải clone mới dữ liệu ra
+       */
+      state.cartItems = state.cartItems.filter((x) => x.id !== idNeedToRemove);
     },
   },
 });
