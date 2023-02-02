799448d9a0b5de90db887d793ad6ebee72f3ab16
Nghiem Pham <nghiemph95@gmail.com>
2023-02-03 05:38:18 +0700

    Updated: 03-02-2023 05:38:18

--- 13_React_TypeScript/learn-easy-react/src/App.tsx
+++ 13_React_TypeScript/learn-easy-react/src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import { Student } from './features/labs/Student';
 
 function App() {
-  return <div>Nghiem</div>;
+  return (
+    <div>
+      <Student name="Nghiem" age={18} isHero hobbyList={['eat', 'sleep', 'code']} sayHello={() => {}} />
+    </div>
+  );
 }
 
 export default App;
--- 13_React_TypeScript/learn-easy-react/src/features/labs/Student.tsx
+++ 13_React_TypeScript/learn-easy-react/src/features/labs/Student.tsx
@@ -2,8 +2,16 @@ import * as React from 'react';
 
 export interface StudentProps {
   name?: string;
+  age: number;
+  isHero?: boolean;
+  hobbyList: string[]; // mảng các string
+  sayHello: () => void; // func ko nhận tham số và cũng ko trả về gì cả
 }
 
-export function Student({ name = '' }: StudentProps) {
-  return <div>Student: {name}</div>;
+export function Student({ name = '', isHero = false }: StudentProps) {
+  return (
+    <div>
+      Student: {name} {isHero ? 'hero' : 'no-hero'}
+    </div>
+  );
 }
