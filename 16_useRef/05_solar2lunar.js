cfdda887b2fcb6ca53643b7482b1f0ebe37bcae1
Nghiem Pham <nghiemph95@gmail.com>
2023-02-11 22:33:43 +0700

    Updated: 11-02-2023 22:33:43

---
+++ 16_useRef/05_solar2lunar.js
@@ -0,0 +1,6 @@
+//npm i solar2lunar
+import calendar from "solar2lunar";
+// const calendar = require("solar2lunar");
+// const { solar2lunar, solar2lunar } = require('solar2lunar')
+
+console.log(calendar.solar2lunar(2023, 3, 21));
