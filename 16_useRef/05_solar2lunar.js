e3699bc5ca35fac092387bd3274e8fb2dccf4d09
Nghiem Pham <nghiemph95@gmail.com>
2023-02-12 10:53:01 +0700

    Updated: 12-02-2023 10:53:01

--- 16_useRef/05_solar2lunar.js
+++ 16_useRef/05_solar2lunar.js
@@ -3,4 +3,4 @@ import calendar from "solar2lunar";
 // const calendar = require("solar2lunar");
 // const { solar2lunar, solar2lunar } = require('solar2lunar')
 
-console.log(calendar.solar2lunar(2023, 3, 21));
+console.log(calendar.solar2lunar(2025, 3, 28));
---
+++ 16_useRef/06_lunarTypescript.js
@@ -0,0 +1,10 @@
+// let moonTime = require("moon-time");
+import moonTime from "moon-time";
+
+let moonTimes = moonTime({
+  year: 2025,
+  month: 3,
+  day: 28,
+});
+
+console.log(moonTimes);
