c0d00c13e02981b34b100585d89e1bf2c5ccb0c0
Nghiem Pham <nghiemph95@gmail.com>
2023-02-28 23:35:13 +0700

    Updated: 28-02-2023 23:35:13

--- 16_useRef/06_lunarTypescript.js
+++ 16_useRef/06_lunarTypescript.js
@@ -10,17 +10,26 @@ function getLunarDay(inputDate) {
     ? inputDate.split("/")
     : "";
 
+  console.log([year, month, day]);
+
   let res = "";
 
-  let moonTimes = moonTime({ day: +day, month: +month, year: +year });
+  // let moonTimes = moonTime({ day: +day, month: +month, year: +year });
+  let moonTimes = moonTime({
+    year: +year,
+    month: +month,
+    day: +day,
+  });
+
+  console.log(moonTimes);
 
   for (const time in moonTimes) {
-    let word = `${moonTimes[time]}/`;
+    let word = `${moonTimes[time]}-`;
 
     res = res + word;
   }
 
-  return res.slice(0, -1);
+  return res.slice(0, -12);
 }
 
-console.log(getLunarDay("22/3/2023"));
+console.log(getLunarDay("2023-03-18"));
