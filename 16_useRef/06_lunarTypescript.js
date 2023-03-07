fd31a85894f18902bb9395c4608d19048eb3c343
Nghiem Pham <nghiemph95@gmail.com>
2023-03-07 09:21:05 +0700

    Updated: 07-03-2023 09:21:05

--- 16_useRef/06_lunarTypescript.js
+++ 16_useRef/06_lunarTypescript.js
@@ -4,26 +4,39 @@ import moonTime from "moon-time";
 function getLunarDay(inputDate) {
   if (!inputDate) return "";
 
+  // // First check for the pattern
+  // var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
+
+  // if ((!regex_date.test(inputDate))) {
+  //   return "";
+  // }
+
   const [year, month, day] = inputDate.includes("-")
     ? inputDate.split("-")
     : inputDate.includes("/")
     ? inputDate.split("/")
     : "";
 
+  // Check the ranges of month and year
+  if (year < 2000 || year > 2100 || month === 0 || month > 12) {
+    return "";
+  }
+
+  console.log(year);
   console.log([year, month, day]);
 
   let result = "";
   let moonTimes = moonTime({
-    year: +year,
-    month: +month,
     day: +day,
+    month: +month,
+    year: +year,
   });
 
   console.log(moonTimes);
 
   for (const time in moonTimes) {
     let word =
-      moonTimes[time] < 10 ? `0${moonTimes[time]}-` : `${moonTimes[time]}-`;
+      moonTimes[time] < 10 ? `0${moonTimes[time]}/` : `${moonTimes[time]}/`;
 
     result = result + word;
   }
